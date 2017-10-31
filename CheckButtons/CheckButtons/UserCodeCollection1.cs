ce4c8774f33e09aba2454f2a04b3b8fb4fd17ed7
GeorgiVatashki <peter.koniarov@engview.com>
2017-10-31 09:57:11 +0200

    Added all files

---
+++ CheckButtons/CheckButtons/UserCodeCollection1.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by Ranorex
+ * User: EVMX
+ * Date: 10/16/2017
+ * Time: 10:07 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace CheckButtons
+{
+    /// <summary>
+    /// Ranorex User Code collection. A collection is used to publish User Code methods to the User Code library.
+    /// </summary>
+    [UserCodeCollection]
+    public class UserCodeCollection1
+    {
+        // You can use the "Insert New User Code Method" functionality from the context menu,
+        // to add a new method with the attribute [UserCodeMethod].
+    }
+}
